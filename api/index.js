3b0b97292fa5974aa56e5018319dd6ddb6c79edc
Md-Sakib-Rahman <sakibrahman.user@gmail.com>
2026-03-21 05:34:19 +0600

    [added]: index.js in api for vercel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
-const app = require('../src/app');  
+const app = require('../src/app');
 const connectDB = require('../src/config/db');
 
 module.exports = async (req, res) => {
-  await connectDB();
-  
-  return app(req, res);
+  try {
+    await connectDB(); 
+    
+    return app(req, res); 
+  } catch (error) {
+    console.error("Vercel Entry Error:", error);
+    res.status(500).json({ message: "Database Connection Failed", error: error.message });
+  }
 };
