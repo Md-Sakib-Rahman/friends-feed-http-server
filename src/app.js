4cda74ba6259a1680b73cd9b202a93b6f44f581d
Md-Sakib-Rahman <sakibrahman.user@gmail.com>
2026-03-21 05:18:18 +0600

    [added] cors options

--- src/app.js
+++ src/app.js
@@ -11,9 +11,16 @@ dotenv.config();
 const app = express();
 
 // Middleware
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || "http://localhost:5173",
+//   credentials: true
+// }));
+
 app.use(cors({
   origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
